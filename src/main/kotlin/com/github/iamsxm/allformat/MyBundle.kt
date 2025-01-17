623cf40c09d3a926af63477c518c407436ee2174
GitHub Action <action@github.com>
2025-01-17 05:22:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/iamsxm/allformat/MyBundle.kt
+++ src/main/kotlin/com/github/iamsxm/allformat/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.iamsxm.allformat
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/iamsxm/allformat/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/iamsxm/allformat/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.iamsxm.allformat.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.iamsxm.allformat.MyBundle
+import com.github.iamsxm.allformat.services.MyProjectService
 import javax.swing.JButton
 
 
